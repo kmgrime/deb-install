d189c2e8379d7a9ef363b33a4f296ef06c29cdaa
Kjell-Magnus Grime <kmgrime@gmail.com>
2024-01-25 21:02:49 +0100

    initial commit

---
+++ nerdfonts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir ~/.local/share/fonts
+
+cd /tmp
+fonts=( 
+"FiraCode" 
+"Go-Mono" 
+"Hack" 
+"UbuntuMono" 
+"Mononoki" 
+"SourceCodePro" 
+)
+
+for font in ${fonts[@]}
+do
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/$font.zip
+    unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    rm $font.zip
+done
+fc-cache
